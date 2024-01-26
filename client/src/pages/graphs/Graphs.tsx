88284fc2e6fc7ed7b4acee19b8da9d4022847a6a
Yaroslava Suspitsina <yaroslava.1998@mail.ru>
2024-01-26 15:19:42 +0400

    Fix pie chart

--- client/src/pages/graphs/Graphs.tsx
+++ client/src/pages/graphs/Graphs.tsx
@@ -6,44 +6,16 @@ import { PieChart } from "@mui/x-charts/PieChart";
 import { getDataByCategoryRequest } from "shared/api";
 import { makeSumm } from "shared/lib/helpers";
 import { State } from "shared/types";
-import ColorBlock from "shared/ui/ColorBlock";
 
 import { DateFilters } from "entities/date-filters";
 
 import * as Styled from "./styles";
-
-const Legends = ({ items }: any) => {
-    return (
-        <table width='100%' style={{ paddingTop: 30 }}>
-            {items.map((item: any) => (
-                <tr>
-                    <Styled.CustomTd>
-                        <ColorBlock width={10} height={10} color={item.color} />
-                    </Styled.CustomTd>
-                    <Styled.CustomTd>{item.title}</Styled.CustomTd>
-                    <Styled.CustomTd>{item.percent}%</Styled.CustomTd>
-                    <Styled.CustomTd float='right'>
-                        {makeSumm(item.summ)}
-                    </Styled.CustomTd>
-                </tr>
-            ))}
-        </table>
-    );
-};
 
 export const Graphs = () => {
     const dataGraphs = useSelector((state: State) => state.dataGraphs);
     const userData = useSelector((state: State) => state?.userData);
 
     const { data, totalSumm, startDate, endDate } = dataGraphs;
-    const legendsItems = data.map((el: any) => ({
-        title: el.label,
-        strokeWidth: 10,
-        category: el.category,
-        color: el.color,
-        summ: el.summ,
-        percent: el.angle,
-    }));
 
     const handleOnSave = ({ startDate, endDate }: any) => {
         getDataByCategoryRequest(userData, {
@@ -51,6 +23,7 @@ export const Graphs = () => {
             endDate,
         });
     };
+
     return (
         <Styled.Container>
             <DateFilters
@@ -61,17 +34,20 @@ export const Graphs = () => {
             <span>Expenses: {makeSumm(totalSumm)}</span>
             <Styled.ChartContainer>
                 {data?.length != 0 ? (
-                    <PieChart width={300} height={300} series={data} />
+                    <PieChart
+                        width={500}
+                        height={300}
+                        series={[
+                            {
+                                data: data,
+                                highlightScope: {
+                                    faded: "global",
+                                    highlighted: "item",
+                                },
+                            },
+                        ]}
+                    />
                 ) : null}
-                {/* <RadialChart
-          colorType={"literal"}
-          colorDomain={[0, 100]}
-          colorRange={[0, 10]}
-          data={data}
-          width={300}
-          height={300}
-        /> */}
-                <Legends items={legendsItems} />
             </Styled.ChartContainer>
         </Styled.Container>
     );
